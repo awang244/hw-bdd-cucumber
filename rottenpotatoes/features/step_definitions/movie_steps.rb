682fe6e8f542c681b8ffe331ffbfa808a96fc662
Allison Wang <awang24@berkeley.edu>
2020-11-06 03:44:11 +0000

    part 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,13 @@
 # Add a declarative step here for populating the DB with movies.
 
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+      Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
+#   fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
